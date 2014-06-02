e98234d094794f5d229d7c7f5ca91e6d9d002aa3
ec2-user <ec2-user@ip-172-31-2-166.ap-northeast-1.compute.internal>
2014-06-02 12:01:57 -0400

    Fix gulpfile settings

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,18 @@
 var gulp = require('gulp')
+var gutil = require('gulp-util')
 var coffee = require('gulp-coffee')
 var concat = require('gulp-concat')
 
 gulp.task('build', function () {
   gulp.src('src/*.coffee')
-    .pipe(concat('mmd-concat.coffee'))
+    .pipe(concat('MMD.coffee'))
     .pipe(gulp.dest('.'))
-    .pipe(coffee({bare: true}))
-    .pipe(gulp.dest('MMD.js'))
+    .pipe(coffee({bare: true}).on('error', gutil.log))
+    .pipe(gulp.dest('.'))
 })
 
 gulp.task('watch', function () {
-  gulp.src('src/*.coffee', ['build'])
+  gulp.watch('src/*.coffee', ['build'])
 })
 
 gulp.task('default', ['build'])
